556e0dc6d2bb26a12da9689e19167c7b2ae457c1
K Abhi <abhi543it@gmail.com>
2024-09-18 12:47:43 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY13/sample.js
@@ -0,0 +1,93 @@
+// if else if ladder
+// let marks=window.prompt("Enter Your Marks:");
+// if(marks>=85 && marks<=100){
+//     console.log("You got A+ grade");
+// }
+// else if(marks>=70 && marks<=84){
+//      console.log("B grade");
+// }
+// else if(marks>=50 && marks<69){
+//      console.log("c grade")
+// }
+// else if(marks>=35 && marks<49){
+//     console.log("D grade")
+// }
+// else if(marks<35){
+//      console.log("sorry u r failer")
+// }
+// else{
+//      alert("ENTER THE VALID MARKS");    
+// }
+
+// months and their seasons
+// let month=window.prompt("Enter the month:");
+// if(month=="december" || month=="january" || month=="febuary"){
+//      console.log("WINTER SEASON");
+    
+// }
+// else if(month=="march" || month=="april"){
+//      console.log("SUMMER");
+// }
+// else if(month=="june" || month=="july" || month=="september"){
+//      console.log("Monsoon");
+// }
+// else if(month=="octomber" || month=="november"){
+//      console.log("POST-MONSOON");
+// }
+// else{ 
+//       alert("Enter the correct month name || Every letter of the month should be in small letters");
+
+// }
+
+// create a system to calculate popcorn prices based on the size of the customer ask for:
+// let user_size=window.prompt("Enter the size:");
+// if(user_size=="XL"){
+//      console.log("PRIZE : 250Rs");
+// }
+// else if(user_size=="L"){
+//     console.log("PRIZE : 200Rs");
+// }
+// else if(user_size=="M"){
+//     console.log("PRIZE : 100Rs");
+// }
+// else if(user_size=="S"){
+//     console.log("PRIZE : 50Rs");
+// }
+// else{
+//     alert("ENTER THE VALID SIZE!");
+// }
+
+// enter 1-7 find the day
+// let day=window.prompt("Enter the number from 1-7");
+// switch(day){
+//     case "1":
+//         console.log("MONDAY");
+//         break;
+//     case "2":
+//         console.log("TUESDAY");
+//         break; 
+//     case "3":
+//         console.log("WEDNESDAY");
+//         break;
+//         case "4":
+//             console.log("THURSDAY");
+//             break;
+//         case "5":
+//             console.log("FRIDAY");
+//             break; 
+//         case "6":
+//             console.log("SATURDAY");
+//             break; 
+//             case "7":
+//                 console.log("SUNDAY");
+//                 break;
+//         default:
+//             alert("ENTER THE NO. FROM 1 - 7 ONLY!")              
+// 
+let good_string="Pbhi";
+if(good_string.startsWith("a") && good_string.length>3){
+     console.log("GOOD STRING");
+} 
+else{
+     console.log("BAD STRING");
+}
