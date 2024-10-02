0de5829de4df12ddb5a360b0b8166e8f184d928d
K Abhi <abhi543it@gmail.com>
2024-10-02 12:17:32 +0530

    Update default_arguments,spread.js

--- JAVASCRIPT/DAY 19/default_arguments,spread.js
+++ JAVASCRIPT/DAY 19/default_arguments,spread.js
@@ -15,3 +15,30 @@ let r=[1,2,3,4,5];
 const [one,two,...rest]=r;// rest holds arr
 console.log(one,two);
 console.log(rest)
+
+
+// Spread with arrays
+let arr=[1,2,3,4,5];
+console.log(...arr);
+let chars=[..."geetha"];
+console.log(chars);
+
+let even=[2,4,6,8];
+let odd=[1,3,5,7,9];
+let add_array=[...even,...odd];
+console.log(add_array);
+
+// spread with objects
+const obj={
+   name:"Ramesh",
+   age:20
+};
+console.log({...obj,roll:90,email:"abhi123"});
+
+// converting array into objects
+let array=[1,2,3,4,5];
+let convert={...array};
+console.log(convert);
+
+let object={..."Abhi"};
+console.log(object);
