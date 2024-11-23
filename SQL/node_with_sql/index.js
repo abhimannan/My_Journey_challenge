9d23c510aac3f5900d285a205a57c306f7b30ac4
abhimannan <abhi543it@gmail.com>
2024-11-23 09:33:17 +0530

    database connection

--- SQL/node_with_sql/index.js
+++ SQL/node_with_sql/index.js
@@ -4,26 +4,56 @@ const mysql = require('mysql2');
 
 // Create the connection to database
 const connection = mysql.createConnection({
- host: 'localhost',
- user: 'root',
- database: 'practice',
- password :"geetha143abhi"
+  host: 'localhost',
+  user: 'root',
+  database: 'delta',
+  password: "geetha143abhi"
 });
 
-// A simple SELECT query
-try{
+// A simple INSERT query
+let q = `INSERT INTO student VALUES ?`;
+// let users = [
+//   ["1254", "abhi", "abhi@gmail.com", "1431"],
+//   ["1235", "abhi", "abhi@gmail.com", "148931"]
+// ];
+// let users2 = [
+//   ["9652", "vijay", "vijay@gmail.com", "14511"],
+//   ["2356", "kumar", "kuar@gmail.com", "98565"]
+// ];
+function getdata(){
+  return [
+    faker.string.uuid(),
+     faker.internet.username(), // before version 9.1.0, use userName()
+     faker.internet.email(),
+    faker.internet.password(),
+    ];
+}
+let data=[];
+for(let i=1;i<=50;i++){
+   data.push(getdata());
+}
+// Execute the query
 connection.query(
- "show databases",
- function (err, results, fields) {
-   console.log(results); // results contains rows returned by server
-   console.log(fields); // fields contains extra meta data about results, if available
- }
+  q, [data],
+  function (err, results, fields) {
+    if (err) {
+      console.error("Error executing query:", err);
+    } else {
+      console.log("Results:", results);
+    }
+
+    // Close the connection after the query
+    connection.end((endErr) => {
+      if (endErr) {
+        console.error("Error closing the connection:", endErr);
+      } else {
+        console.log("Connection closed.");
+      }
+    });
+  }
 );
-}
-catch(error){
-  console.log(error);
-}
-connection.end;// to close the connection
+
+
 
 
 let createRandomUser = ()=> {
---
+++ SQL/node_with_sql/schema.sql
@@ -0,0 +1,19 @@
+-- show tables;
+-- -- show databases;
+-- select * from emp_info;
+-- select emp_name,salary from emp_info;
+use delta;
+-- show databases;
+-- drop table if exists user;
+-- drop table if exists user1;
+-- drop table if exists students;
+
+create table user(
+id int primary key,
+username varchar(30),
+email varchar(30),
+pass_word varchar(40)
+);
+-- create table products(id int,name varchar(30));
+select * from user;
+-- show tables;
