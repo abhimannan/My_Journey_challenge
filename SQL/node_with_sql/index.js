633643cb294677cdfc7b5dd50c5f3275d1d3811d
abhimannan <abhi543it@gmail.com>
2024-11-24 20:26:55 +0530

    Routing

--- SQL/node_with_sql/index.js
+++ SQL/node_with_sql/index.js
@@ -1,6 +1,18 @@
+let express=require("express");
+let app=express();
+let port=9090;
+let path=require("path");
 const { faker } = require('@faker-js/faker');
 // Get the client
 const mysql = require('mysql2');
+
+// middlewares
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -9,9 +21,22 @@ const connection = mysql.createConnection({
   database: 'delta',
   password: "geetha143abhi"
 });
-
+// generate the random array
+function getdata() {
+  return [
+   faker.string.uuid(),
+   faker.internet.username(), // before version 9.1.0, use userName()
+   faker.internet.email(),
+   faker.internet.password(),
+  ];
+ };
 // A simple INSERT query
 let q = `INSERT INTO student VALUES ?`;
+let data=[];
+for(let i=0;i<=100;i++){
+  data.push(getdata());
+}
+// let user=["1254", "abhi", "abhi@gmail.com", "1431"];
 // let users = [
 //   ["1254", "abhi", "abhi@gmail.com", "1431"],
 //   ["1235", "abhi", "abhi@gmail.com", "148931"]
@@ -20,28 +45,17 @@ let q = `INSERT INTO student VALUES ?`;
 //   ["9652", "vijay", "vijay@gmail.com", "14511"],
 //   ["2356", "kumar", "kuar@gmail.com", "98565"]
 // ];
-function getdata(){
-  return [
-    faker.string.uuid(),
-     faker.internet.username(), // before version 9.1.0, use userName()
-     faker.internet.email(),
-    faker.internet.password(),
-    ];
-}
-let data=[];
-for(let i=1;i<=50;i++){
-   data.push(getdata());
-}
+
+
 // Execute the query
 connection.query(
-  q, [data],
+  q,[data],
   function (err, results, fields) {
     if (err) {
       console.error("Error executing query:", err);
     } else {
-      console.log("Results:", results);
+      // console.log("Results:", results);
     }
-
     // Close the connection after the query
     connection.end((endErr) => {
       if (endErr) {
@@ -52,15 +66,32 @@ connection.query(
     });
   }
 );
+// createing route
+app.get("/home",(req,resp)=>{
+  let len_data=data.length;
+  // // get all id's
+  // for(let i of data){
+  //    console.log(i[0]);
+  // }
+  //get all uernames
+  for(let i of data){
+    console.log(`Username = ${i[1]}`);
+ }
+//  // get all emails
+//  for(let i of data){
+//   console.log(i[2]);
+// }
+// // get all passwords
+// for(let i of data){
+//   console.log(i[3]);
+// }
+  // console.log("id = ",data[0]);
+   resp.render("user.ejs",{len_data});
+});
+
+app.listen(port,(req,resp)=>{
+  console.log(`The Server is Running in port ${port}`);
+});
 
 
 
-
-let createRandomUser = ()=> {
- return {
-   id: faker.string.uuid(),
-   username: faker.internet.username(), // before version 9.1.0, use userName()
-   email: faker.internet.email(),
-   password: faker.internet.password(),
- };
-}
