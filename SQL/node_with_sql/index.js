4b5e1a3f59642a52f339408da78ebcfbcb03a766
abhimannan <abhi543it@gmail.com>
2024-11-26 18:51:34 +0530

    sql

--- SQL/node_with_sql/index.js
+++ SQL/node_with_sql/index.js
@@ -3,6 +3,8 @@ let app=express();
 let port=9090;
 let path=require("path");
 const { faker } = require('@faker-js/faker');
+// below package is for delete the posts
+var methodOverride = require('method-override');
 // Get the client
 const mysql = require('mysql2');
 
@@ -12,7 +14,8 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -31,11 +34,11 @@ function getdata() {
   ];
  };
 // A simple INSERT query
-let q = `INSERT INTO student VALUES ?`;
+/*let q = `INSERT INTO student VALUES ?`;
 let data=[];
-for(let i=0;i<=100;i++){
+for(let i=0;i<2;i++){
   data.push(getdata());
-}
+}*/
 // let user=["1254", "abhi", "abhi@gmail.com", "1431"];
 // let users = [
 //   ["1254", "abhi", "abhi@gmail.com", "1431"],
@@ -48,49 +51,98 @@ for(let i=0;i<=100;i++){
 
 
 // Execute the query
-connection.query(
-  q,[data],
-  function (err, results, fields) {
-    if (err) {
-      console.error("Error executing query:", err);
-    } else {
-      // console.log("Results:", results);
-    }
-    // Close the connection after the query
-    connection.end((endErr) => {
-      if (endErr) {
-        console.error("Error closing the connection:", endErr);
-      } else {
-        console.log("Connection closed.");
-      }
-    });
-  }
-);
+// connection.query(
+//   q,[data],
+//   function (err, results) {
+//     if (err) {
+//       console.error("Error executing query:", err);
+//     } else {
+//       // console.log("Results:", results);
+//     }
+//     // Close the connection after the query
+//     connection.end((endErr) => {
+//       if (endErr) {
+//         console.error("Error closing the connection:", endErr);
+//       } else {
+//         console.log("Connection closed.");
+//       }
+//     });
+//   }
+// );
 // createing route
-app.get("/home",(req,resp)=>{
-  let len_data=data.length;
-  // // get all id's
-  // for(let i of data){
-  //    console.log(i[0]);
-  // }
-  //get all uernames
-  for(let i of data){
-    console.log(`Username = ${i[1]}`);
- }
-//  // get all emails
-//  for(let i of data){
-//   console.log(i[2]);
-// }
-// // get all passwords
-// for(let i of data){
-//   console.log(i[3]);
-// }
-  // console.log("id = ",data[0]);
-   resp.render("user.ejs",{len_data});
+app.get("/home", (req, resp) => {
+  let q = `SELECT COUNT(*) AS student_count FROM student`;
+    connection.query(q, (err, results) => {
+      if (err) {
+        console.error(err);
+        return resp.status(500).send("Error in DB");
+      }
+      let t=results[0].student_count;
+      resp.render("user.ejs",{t});
+      resp.send("results"); // Send the results back to the client.
+    });
+  
+});
+
+// app.get("/users",(req,resp)=>{
+//    resp.render("all_details.ejs",{data});
+// });
+
+// app.get("/users/:id",(req,resp)=>{
+//    let {id}=req.params;
+//    let single_data=data.find((d)=>{
+//       if(id==d[0]){
+//          return d;
+//       }
+//    });
+//    resp.render("single_data.ejs",{single_data});
+// });
+// app.get("/users/edit/:id",(req,resp)=>{
+//   let {id}=req.params;
+//    let edit_data=data.find((d)=>{
+//       if(id==d[0]){
+//          return d;
+//       }
+//    });
+//    resp.render("edit.ejs",{edit_data});
+   
+// });
+// app.post("/users",(req,resp)=>{
+//   // let id=uuid();
+//     let {id,username,email,pin}=req.body;
+//     console.log(id,username,email,pin);
+//     data[0][0]=id;
+//     data[0][1]=username;
+//     data[0][2]=email;
+//     data[0][3]=pin;
+//     console.log("data is edited");
+//     resp.redirect("http://localhost:9090/users");
+// });
+// // add data
+// app.get("/new",(req,resp)=>{
+//   resp.render("new.ejs");
+// });
+// app.post("/new",(req,resp)=>{
+//  let new_post=getdata();
+//    data.push(new_post);
+//    resp.redirect("http://localhost:9090/users");
+// });
+
+// app.delete("/delete/:id", (req, resp) => {
+//   let { id } = req.params;
+//   data = data.filter((p) => id !== String(p[0])); // Converts id to string to match comparison
+//   console.log(data)
+//   resp.redirect("/users");
+// });
+
+
+app.get("*",(req,resp)=>{
+   resp.send(("Page Not Found"));
 });
 
 app.listen(port,(req,resp)=>{
   console.log(`The Server is Running in port ${port}`);
+  // console.log(data);
 });
 
 
