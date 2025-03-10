b30f071e1afd6ae7845f5b2b9f73d49c4b8362ed
Abhi <abhi543it@gmail.com>
2025-03-10 07:20:58 +0530

    sub arrays max sum

--- java/Arrays_concepts/Arrays_Part2/Problems.java
+++ java/Arrays_concepts/Arrays_Part2/Problems.java
@@ -1,23 +1,32 @@
 package Arrays_Part2;
-import java.util.ArrayList;
+
 
 
 public class Problems {
-    public static void main(String[] args) {
-        int[] arr = {1,-2,6,-1,3};
+    // function for find the sum largest number
+    public static void maxSum(int[] arr){
+        int sum = 0;
+        int max_Value = Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
             for(int j=i;j<arr.length;j++){
-                int sum = 0;
+                sum = 0;
                 for(int k=i;k<=j;k++){
-                    sum=sum+arr[k];
-                    // System.out.print(arr[k]+" ");
-                    
+                    sum+=arr[k];
                 }
                 System.out.print(sum+"  ");
+                if(max_Value<sum){
+                    max_Value=sum;
+                }
+                System.out.println();
             }
-            
-            System.out.println();
         }
+        System.out.print("max value : "+max_Value);
+    }
+    public static void main(String[] args) {
+        
+        int[] arr = {1,-2,6,-1,3};
+        maxSum(arr);
+        
 
     }
 }
