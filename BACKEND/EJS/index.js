0e094a6d764750998098c3a88be831598014dfd8
Abhi <abhi543it@gmail.com>
2025-03-17 19:57:51 +0530

    instagram sample

--- BACKEND/EJS/index.js
+++ BACKEND/EJS/index.js
@@ -3,12 +3,13 @@ let app = express();
 let port = 9595;
 let path = require("path");
 
+
+
 // middlewares for set the ejs files
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));// it will give EJS/views/home.ejs
 app.use(express.static(path.join(__dirname,"public")));
-
-
+app.use(express.static(path.join(__dirname,"/public/javascript")));
 
 app.get("/home",(req,resp)=>{
      resp.render("home.ejs");
@@ -19,6 +20,21 @@ app.get("/random",(req,resp)=>{
      let num = Math.floor(data);
      resp.render("random.ejs",{num});
 });
+
+app.get("/ig/:username",(req,resp)=>{
+     let {username} = req.params;
+     let instaData = require("./data.json");
+     let data = instaData[username];
+     console.log(data);
+     resp.render("instagram.ejs",{data});
+     
+}); 
+
+
+app.get("/instagram",(req,resp)=>{
+     
+});
+
 
 app.listen(port,()=>{
      console.log(`The server i running at port ${port}`);
