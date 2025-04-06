dff57991b48d705ccbc956e374e2c9ee59aeb56a
Abhi <abhi543it@gmail.com>
2025-04-07 05:28:27 +0530

    find the number of vowels in a string

---
+++ java/Strings/Assignment/VowerlsCount.java
@@ -0,0 +1,25 @@
+package Strings.Assignment;
+// Questions :
+// Count how many times lowercase vowels occurred in a String entered by the user
+
+import java.util.*;
+
+public class VowerlsCount {
+    public static void CountLowercaseLetters(String str){
+        int count = 0;
+        for(int i=0;i<str.length();i++) {
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u') {
+                count++;
+            }
+        }
+        System.out.println("No. of vowels = "+ count);
+    }
+
+    public static void main(String[] args) {
+        // String str = "Abhi";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string : ");
+        String str = sc.nextLine();
+        CountLowercaseLetters(str);
+    }
+}
