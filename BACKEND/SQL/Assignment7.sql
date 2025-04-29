54b87743a5bec2fc5f6140a9077a0353f2e67343
Abhi <abhi543it@gmail.com>
2025-04-29 19:55:34 +0530

    SQL practice

--- BACKEND/SQL/Assignment7.sql
+++ BACKEND/SQL/Assignment7.sql
@@ -17,6 +17,8 @@ insert into students values(1,"vijay",18),(2,"abhi",20);
 select * from students
 where age between 18 and 22;
 select * from students
+where name not in("abhi");
+select * from students
 where age in(18,22);
 create table products(
 id int,
@@ -24,6 +26,8 @@ product_name varchar(50),
 price int
 );
 insert into products values(1,"shirt",350),(1,"pant",550);
+select * from products
+where price between 300 and 600;
 select * from products
 where price > 100;
 create table customers(
@@ -34,3 +38,24 @@ country varchar(70)
 insert into customers values(1,"geetha","new yark"),(2,"kumar","new yark");
 select name from customers
 where country = "new yark";
+create table books(
+id int,
+book_name varchar(100),
+published date
+);
+insert into books values(2,"Atomic habits","2025-10-10");
+select * from books
+where published > "2024-10-10";
+select * from employees;
+select * from employees
+where name like "a%";
+select * from employees
+where name like "%a";
+create table orders(
+id int,
+product_name varchar(100),
+order_date date
+);
+insert into orders values (1,"pencial","2021-01-01");
+select * from orders
+where order_date < "2022-01-01";
