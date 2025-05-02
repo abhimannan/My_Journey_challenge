ecc964b68271bb7e7832443b1d1add828c8285b4
Abhi <abhi543it@gmail.com>
2025-05-02 19:48:37 +0530

    limit clause

--- BACKEND/SQL/Assignment9.sql
+++ BACKEND/SQL/Assignment9.sql
@@ -1,6 +1,5 @@
-create database Assignment8;
-use Assignment8;
-drop table employees;
+create database Assignment9;
+use Assignment9;
 create table employees (
 emp_id int,
 emp_name varchar(50),
@@ -19,23 +18,31 @@ insert into employees values(2,"priya","IT",60000,32,"priya123@gmail.com","2021-
                             (5,"Kiran","IT",70000.00,35,null,"2019-04-15"),
                             (6,"Sneha","Marketing",30000.00,22,"sneha@rediffmail.com","2020-11-10");
 select * from employees
-where depertment in("IT");
-drop table employees;
+order by salary
+limit 3;
+select * from employees
+where hire_date > "2022-01-01"
+limit 2;
+select * from employees
+order by age
+limit 2 offset 2;
+select * from employees
+where depertment in("HR")
+limit 4;
+select * from employees
+where depertment in("Marketing")
+limit 1;
+select * from employees
+where age > 30
+order by salary desc;
+select * from employees
+where salary > 35000
+limit 3 offset 1;
 select emp_name from employees
-where salary > 50000;
-select * from employees
-where age < 30;
-select * from employees
-where email like "%gmail.com";
-select emp_name,salary from employees
-where salary between 30000 and 50000;
-select * from employees
-where depertment not in("HR","Sales");
-select * from employees
-where email like "null";
-select * from employees
-where hire_date in('2022-03-12');
-select * from employees
-where emp_name like "S%";
-select * from employees
-where age not between 25 and 32;
+where age < 28
+limit 5;
+select * from employees 
+where depertment in("Sales")
+limit 1;
+
+
