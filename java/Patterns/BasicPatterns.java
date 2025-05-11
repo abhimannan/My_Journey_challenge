7526bba46717c2b0e89d3328ea1362eacc64ebba
Abhi <abhi543it@gmail.com>
2025-05-11 12:11:49 +0530

    pattern programs practice

--- java/Patterns/BasicPatterns.java
+++ java/Patterns/BasicPatterns.java
@@ -106,7 +106,8 @@ public class BasicPatterns {
                 }
                 System.out.println();
             }
-                */
+            */
+            /*
             int n=8;
             for(int i=1;i<=4;i++) {
                 //stars
@@ -138,8 +139,155 @@ public class BasicPatterns {
                 }
                 System.out.println();
             }
-
-    
+            */
+            /*
+            int n = 5;
+            for(int i=1;i<=n;i++) {
+                // spaces
+                for(int sp=n-i;sp>=1;sp--) {
+                    System.out.print(" ");
+                } 
+                // stars
+                for(int sr=1;sr<=n;sr++) {
+                    if(i==1 || i==n || sr==1 || sr==n) {
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+            */
+            /*
+            int n = 5;
+            for(int i=1;i<=5;i++) {
+                for(int col=1;col<=i;col++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for(int row=1;row<=n-1;row++) {
+                for(int col=n-row;col>=1;col--) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            */
+            /*
+            int n = 5;
+            for(int i=1;i<=n;i++) {
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                for(int st=1;st<=i;st++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            */
+            /*
+            int n = 5;
+            for(int i=1;i<=n;i++) {
+                // spaces
+                for(int sp=1;sp<=(i-1);sp++) {
+                    System.out.print(" ");
+                }
+                for(int st=1;st<=(n-i+1);st++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            */
+            /*
+            int n = 5;
+            for(int i=1;i<=n;i++) {
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                for(int st=1;st<=(i*2-1);st++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for(int i=n;i>=1;i--) {
+                // spaces
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                // stars
+                for(int st=1;st<=(i*2-1);st++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            */
+            /*
+            int n = 5;
+            for(int i=n;i>=1;i--) {
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                for(int st=1;st<=i;st++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            for(int i=1;i<=n;i++) {
+                // spaces
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                // stars
+                for(int st=1;st<=i;st++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            */
+            int n =6;
+            /*
+            for(int i=1;i<=n;i++) {
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=(i*2-1);k++) {
+                    if( k==1 || k==(i*2-1)){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                    
+                }
+                System.out.println();
+            }
+            for(int i=n;i>=1;i--) {
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                for(int k=(i*2-1);k>=1;k--) {
+                    if( k==1 || k==(i*2-1)){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                    
+                }
+                System.out.println();
+            }
+            */
+            for(int i=1;i<n;i++) {
+                for(int sp=1;sp<=(n-i);sp++) {
+                    System.out.print(" ");
+                }
+                for(int k=1;k<=i;k++) {
+                    
+                    
+                }
+                System.out.println();
+            }
 
         }
     }
