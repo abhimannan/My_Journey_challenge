7c64074f3bee02edd08a959c56221c62dd73f099
Abhi <abhi543it@gmail.com>
2025-05-25 08:23:54 +0530

    middlewares

--- BACKEND/mongo/Module_6/Middlewares/index.js
+++ BACKEND/mongo/Module_6/Middlewares/index.js
@@ -1,10 +1,28 @@
 let express = require("express");
 let app = express();
 
-app.use()
+/*
+app.use((req,resp,next)=>{
+    console.log("This is my 1st middleware");
+    next();
+});
+app.use((req,resp,next)=>{
+    console.log("This is my 2nd middleware");
+    next();
+});
+*/
+ // logger - Morgan
+app.use((req,resp,next)=>{
+    console.log(req.hostname,req.method,req.url);
+    next();
+});
+
+app.get("/",(req,resp)=>{
+    resp.send("Root route");
+});
 
 app.get("/home",(req,resp)=>{
-    resp.send("home route");
+    resp.send("Home route");
 });
 
 app.listen(5252,()=>{
