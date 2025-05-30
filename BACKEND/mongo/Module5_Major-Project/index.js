650733de4140efdc37c0b4667f31054b3c507e3b
Abhi <abhi543it@gmail.com>
2025-05-30 14:31:22 +0530

    add

--- BACKEND/mongo/Module5_Major-Project/index.js
+++ BACKEND/mongo/Module5_Major-Project/index.js
@@ -4,8 +4,10 @@ let port = 8080;
 const mongoose = require('mongoose');
 let Listing = require("./models/listing.js");
 let path = require("path");
-var methodOverride = require('method-override');
+let methodOverride = require('method-override');
 let ejsmate = require('ejs-mate')
+let ExpressError = require("./utils/ExpressError.js");
+let Wrapasync = require("./utils/Wrapasync.js");
 
 
 main().then((res)=>{
@@ -42,59 +44,73 @@ app.get("/testListing",async (req,resp)=>{
 */
 
 // show route
-app.get("/listings",async (req,resp)=>{
+app.get("/listings",Wrapasync(async (req,resp)=>{
     let data = await Listing.find({});
     resp.render("index.ejs",{data});
-});
+}));
 
 // show individual data
-app.get("/listings/:id",async (req,resp)=>{
+app.get("/listings/:id",Wrapasync(async (req,resp)=>{
     let {id} = req.params;
     let listing =await Listing.findById({_id : id});
     resp.render("show.ejs",{listing});
     // resp.redirect("/listings");
-});
+}));
 
 // new route
 app.get("/Newlistings",(req,resp)=>{
     resp.render("new.ejs");
 });
 // add new data
-app.post("/listings",async(req,resp)=>{
-    // let listing = req.body.listing;
-    let Newdata = new Listing(req.body.listing);
-    await Newdata.save();
-    resp.redirect("/listings");
-});
+app.post("/listings",Wrapasync(async(req,resp,next)=>{
+        let Newdata = new Listing(req.body.listing);
+        await Newdata.save();
+        resp.redirect("/listings");
+}));
 
 // eidt form
-app.get("/listings/:id/edit", async (req, resp) => {
+app.get("/edit/:id", Wrapasync(async (req, resp) => {
      let {id} = req.params;
-     let listing =await Listing.findById({_id : id});
+     let listing =await Listing.findById(id);
      resp.render("edit.ejs",{listing});
-});
+}));
 
 // update route
-app.put("/listings/:id", async (req, resp) => {
+app.put("/listings/:id", Wrapasync(async (req, resp) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    resp.redirect(`/listings/${id}`);
-
-});
+    resp.redirect(`/listings`);
+}));
 
 // destroy route
-app.delete("/listings/:id",async (req,resp)=>{
+app.delete("/listings/:id",Wrapasync(async (req,resp)=>{
     let {id} = req.params;
     await Listing.deleteOne({_id : id});
     resp.redirect("/listings");
-});
+}));
 
 
 app.get("/home",(req,resp)=>{
     resp.send("Home Route");
 });
 
+// if the route is not matched
+app.all("*",(req,resp,next)=>{
+    next(new ExpressError(404,"Page not found"));
+});
+
+// logger
+// app.use((req, res, next) => {
+//   console.log("REQUEST PATH ", req.path);
+//   next();
+// });
+
+// error handling middlewares
+app.use((err,req,resp,next)=>{
+    let {status=500,message="something went wrong"} = err;
+    resp.status(status).send(message);
+});
+
 app.listen(port,(req,resp)=>{
     console.log(`Server is running at port ${port}`);
-});
-                            
+});                    
